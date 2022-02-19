21fc6ff517f04e156cfcb7494416fb6dc9a63197
hoarus <hoarus@gmail.com>
2022-02-19 21:05:32 +1000

    Create navbar

--- src/index.js
+++ src/index.js
@@ -1,2 +1,42 @@
+import './style.css';
+import './normalise.css';
+import elephants_image from './images/elephants_sitting.jpeg';
 
-console.log("Success")
+
+// Create Content // 
+
+const content = document.createElement('div');
+content.setAttribute('id', 'content');
+document.body.appendChild(content);
+
+function generateNavbar() {
+  const nav = document.createElement('nav');
+  nav.setAttribute('class','navbar');
+  content.appendChild(nav);
+
+  createElement(nav, "div", "nav-item", "Home");
+  createElement(nav, "div", "nav-item", "Menu");
+  createElement(nav, "div", "nav-item", "Contact Us");
+}
+
+
+function createElement(parent, type, classname, content) {
+  const newElement = document.createElement(type);
+  newElement.setAttribute('class',classname);
+  newElement.textContent = content;
+  parent.appendChild(newElement);
+}
+
+
+
+generateNavbar();
+
+// Need to spit out the below into the index.js file
+
+const bannerImage = new Image();
+bannerImage.src = elephants_image;
+content.appendChild(bannerImage);
+
+
+
+
